cf9408b45784deefb2c42a6b4f229d8fa5adb58b
absukatidur <andika.azk96@gmail.com>
2025-09-12 14:31:31 +0800

    Update login

--- src/login.php
+++ src/login.php
@@ -1,12 +1,56 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link rel="stylesheet" href="../assets/css/login.css">
-</head>
-<body>
-    <h1>Login</h1>
-</body>
+<!doctype HTML>
+<html>
+    <head>
+        <title>
+            Login Page RM
+        </title>
+        <link rel="stylesheet" href="assets/css/style.css">
+    </head>
+    <body style= "margin: 0px">
+        <div class="container">
+            <p class="logo"> 
+                    Logo 
+            </p>
+            <div class="footer">
+                <div class="footer-left">
+                    <img src="https://logodix.com/logo/498837.png" alt="Copyright Logo" class="copyright-logo">
+                </div>
+                <div class="footer-right">
+                    2025 NamaKelompok
+                </div>
+            </div>
+            <div class="left">
+                <h1 class= "header">
+                    Log In
+                </h1>
+                <p class="welcome-text">
+                    Selamat datang di Website Restoran Minang!
+                </p>
+                <form class="login-form">
+                <label for="email"><b class= "p">Email*</b></label>
+                <input type="email" id="email" placeholder="Masukkan email" class="input-field"style = "margin-bottom: 15px">
+                <div class="label-row">
+                    <label for="password"><b class= "p">Password*</b></label>
+                    <a href="#" class="forgot-password">Forgot Password?</a>
+                </div>
+                <div class="input-container">
+                    <input type="password" id="password" placeholder="Masukan Password" class="input-field">
+                </div>
+                <button class="login-button" type="submit">Log In</button>
+                </form>
+                <div class="login-with-google">
+                    <button class="google-login-button">
+                        <img src="https://logodix.com/logo/267631.png" alt="Google Logo" class="google-logo">
+                        Login with Google
+                    </button>
+                </div>
+                <p class="signup-text">
+                    Don't have an account? <a href="#" class="sign-up-link">Sign Up</a>
+                </p>
+            </div>
+            <div class="right">
+                <img src="https://www.rukita.co/stories/wp-content/uploads/2020/01/restoran-padang-terbaik-di-jakarta-1360x900.png" alt="Login Image" class="login-image">
+            </div>
+        </div>
+    </body>
 </html>
